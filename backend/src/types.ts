ead16570833e41f40f2f7feba510f7199fb06162
Abhishek Rajput <abhi@qwestive.io>
2024-03-19 20:11:04 +0530

    Run prettier

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -1,7 +1,7 @@
 export type SignedMessage = {
-    publicKey: Uint8Array
-    signature: Uint8Array
-    // recoveryId is undefined for ed25519
-    recoveryId: number | undefined
-    fullMessage: Uint8Array
-  }
+  publicKey: Uint8Array;
+  signature: Uint8Array;
+  // recoveryId is undefined for ed25519
+  recoveryId: number | undefined;
+  fullMessage: Uint8Array;
+};
--- backend/src/utils/index.ts
+++ backend/src/utils/index.ts
@@ -4,18 +4,18 @@ import { SecretsManagerClient, GetSecretValueCommand } from "@aws-sdk/client-sec
 const client = new SecretsManagerClient({ region: "us-east-2" }); // Ensure this matches the region of your secret
 
 export async function getSecret(secretName: string) {
-    try {
-        const command = new GetSecretValueCommand({ SecretId: secretName });
-        const response = await client.send(command);
-        if (response.SecretString) {
-            return JSON.parse(response.SecretString);
-        } else {
-            // For binary secrets, use Buffer to decode
-            const buff = Buffer.from(response.SecretBinary, 'base64');
-            return JSON.parse(buff.toString('ascii'));
-        }
-    } catch (err) {
-        console.error(`Error getting secret: ${secretName}`, err);
-        throw err;
+  try {
+    const command = new GetSecretValueCommand({ SecretId: secretName });
+    const response = await client.send(command);
+    if (response.SecretString) {
+      return JSON.parse(response.SecretString);
+    } else {
+      // For binary secrets, use Buffer to decode
+      const buff = Buffer.from(response.SecretBinary, "base64");
+      return JSON.parse(buff.toString("ascii"));
     }
+  } catch (err) {
+    console.error(`Error getting secret: ${secretName}`, err);
+    throw err;
+  }
 }
