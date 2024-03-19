392eecdaa23e831d9fb3790a13e22844909b7033
Adrian Timpau <timpau@users.noreply.github.com>
2024-03-20 00:20:59 +0200

    moday style

--- frontend/components/modal/Disclaimer.tsx
+++ frontend/components/modal/Disclaimer.tsx
@@ -10,13 +10,13 @@ type DisclaimerProps = {
 }
 export function Disclaimer({ onAgree, showModal }: DisclaimerProps) {
   const [agreed, setAgreed] = useState(false)
-  return showModal ? (
+  return !showModal ? (
     <ModalWrapper>
-      <div className="relative max-h-[80vh] w-full max-w-[588px] border border-light-35 bg-darkGray1">
+      <div className="relative max-h-[80vh] w-full max-w-[588px] border border-light-25 bg-black bg-opacity-50">
         <h3 className="disclaimer-title">
           {'Supplemental Token Airdrop Terms'}
         </h3>
-        <div className="scrollbar flex max-h-[300px] flex-col gap-3 overflow-auto border-b border-light-35 px-10 py-5 font-body text-base font-light leading-5 tracking-widest">
+        <div className="scrollbar flex max-h-[300px] flex-col gap-3 overflow-auto border-b border-light-25 px-10 py-5 font-body text-base font-light leading-5 tracking-widest">
           <p>
             {`Welcome to the Supplemental Token Airdrop Terms (these “Airdrop Terms”) for the W token airdrop (the “Airdrop”) by Crossware Limited (“Organization”, “we” or “us”). These Airdrop Terms are supplemental to, and incorporate by reference, our general `}
             <Link href="https://wormhole.com/terms" target="_blank">
@@ -29,9 +29,9 @@ export function Disclaimer({ onAgree, showModal }: DisclaimerProps) {
           </p>
           <ol className="claim-ol">
             <li>
-              You agree and acknowledge that you have the sole responsibility and
-              liability for all taxes in connection with your participation in the
-              Airdrop and you should consult a tax advisor.
+              You agree and acknowledge that you have the sole responsibility
+              and liability for all taxes in connection with your participation
+              in the Airdrop and you should consult a tax advisor.
             </li>
             <li>
               You agree and acknowledge that you are solely responsible for
@@ -40,78 +40,78 @@ export function Disclaimer({ onAgree, showModal }: DisclaimerProps) {
             </li>
             <li>
               You agree and acknowledge that you (a) may receive tokens for free
-              via the Airdrop (other than applicable taxes, if any), (b) were not
-              previously promised any tokens, and (c) took no action in
+              via the Airdrop (other than applicable taxes, if any), (b) were
+              not previously promised any tokens, and (c) took no action in
               anticipation of or in reliance on receiving any tokens or an
               Airdrop.
             </li>
             <li>
               Your eligibility to receive Airdrop tokens or participate in the
-              Airdrop is subject to our sole discretion. To the extent you believe
-              you should have received any airdropped tokens based on any
-              documentation or points system released by us from time to time,
-              such documentation does not entitle you to any tokens or to
+              Airdrop is subject to our sole discretion. To the extent you
+              believe you should have received any airdropped tokens based on
+              any documentation or points system released by us from time to
+              time, such documentation does not entitle you to any tokens or to
               participate in the Airdrop and therefore you have no claim for any
               such tokens.  You agree not to engage in any activities that are
-              designed to obtain more Airdrop tokens than you are entitled to. You
-              agree that you are the legal owner of the blockchain address that
-              you use to access or participate in the Airdrop.
+              designed to obtain more Airdrop tokens than you are entitled to.
+              You agree that you are the legal owner of the blockchain address
+              that you use to access or participate in the Airdrop.
             </li>
             <li>
               You agree and acknowledge that you are not a Prohibited Person and
               you will not use a VPN or other tool to circumvent any geoblock or
               other restrictions that we may have implemented for Airdrop
-              recipients. Any such circumvention, or attempted circumvention, may
-              permanently disqualify you from participation in the Airdrop in our
-              discretion.
+              recipients. Any such circumvention, or attempted circumvention,
+              may permanently disqualify you from participation in the Airdrop
+              in our discretion.
             </li>
             <li>
-              To participate in the Airdrop, you will need to connect a compatible
-              third-party digital wallet (“Wallet”). By using a
-              Wallet, you agree that you are using the Wallet under the terms and
-              conditions of the applicable third-party provider of such Wallet.
-              Wallets are not associated with, maintained by, supported by or
-              affiliated with the Organization. When you interact with the
-              Services, you retain control over your digital assets at all times.
-              We accept no responsibility or liability to you in connection with
-              your use of a Wallet, and we make no representations or warranties
-              regarding how the Services will operate or be compatible with any
-              specific Wallet.{" "}
+              To participate in the Airdrop, you will need to connect a
+              compatible third-party digital wallet (“Wallet”). By using a
+              Wallet, you agree that you are using the Wallet under the terms
+              and conditions of the applicable third-party provider of such
+              Wallet. Wallets are not associated with, maintained by, supported
+              by or affiliated with the Organization. When you interact with the
+              Services, you retain control over your digital assets at all
+              times. We accept no responsibility or liability to you in
+              connection with your use of a Wallet, and we make no
+              representations or warranties regarding how the Services will
+              operate or be compatible with any specific Wallet.{' '}
               <strong>
                 The private keys necessary to access the assets held in a Wallet
-                are not held by the Organization. The Organization has no ability
-                to help you access or recover your private keys and/or seed
-                phrases for your Wallet. You are solely responsible for
+                are not held by the Organization. The Organization has no
+                ability to help you access or recover your private keys and/or
+                seed phrases for your Wallet. You are solely responsible for
                 maintaining the confidentiality of your private keys and you are
                 responsible for any transactions signed with your private keys
               </strong>
             </li>
             <li>
-              You agree and acknowledge that if you are unable to claim an Airdrop
-              due to technical bugs, smart contract issue, gas fees, wallet
-              incompatibility, loss of access to a wallet or the keys thereto, or
-              for any other reason, you will have no recourse or claim against us
-              or our affiliates and that we and our affiliates will not bear any
-              liability.
+              You agree and acknowledge that if you are unable to claim an
+              Airdrop due to technical bugs, smart contract issue, gas fees,
+              wallet incompatibility, loss of access to a wallet or the keys
+              thereto, or for any other reason, you will have no recourse or
+              claim against us or our affiliates and that we and our affiliates
+              will not bear any liability.
             </li>
             <li>
               You agree and acknowledge that claiming an Airdrop may require
               reliance on or an integration with third party products (e.g., a
               wallet or an unaffiliated network or blockchain) that we do not 
-              control. In the event that you are unable to access such products or
-              integrations, or if they fail for any reason, and you are unable to
-              participate in an Airdrop or claim Airdrop tokens, you will have no
-              recourse or claim against us or our affiliates and we and our
-              affiliates will not bear any liability.
+              control. In the event that you are unable to access such products
+              or integrations, or if they fail for any reason, and you are
+              unable to participate in an Airdrop or claim Airdrop tokens, you
+              will have no recourse or claim against us or our affiliates and we
+              and our affiliates will not bear any liability.
             </li>
             <li>
               You agree and acknowledge that the regulatory regime governing
               blockchain technologies, cryptocurrencies and other digital assets
               is uncertain, that new regulations or policies may materially
               adversely affect the potential utility or value of such
-              cryptocurrencies and digital assets, and that there are risks of new
-              taxation related to the purchase or sale of cryptocurrencies and
-              other digital assets.
+              cryptocurrencies and digital assets, and that there are risks of
+              new taxation related to the purchase or sale of cryptocurrencies
+              and other digital assets.
             </li>
             <li>
               You agree and acknowledge that cryptocurrencies and other similar
@@ -120,17 +120,18 @@ export function Disclaimer({ onAgree, showModal }: DisclaimerProps) {
             </li>
           </ol>
 
-          <p> 
-            These Airdrop Terms and the General Terms contain the entire agreement
-            between you and the Organization regarding the Airdrop, and supersede
-            all prior and contemporaneous understandings between the parties
-            regarding the Airdrop. We may modify these Airdrop Terms from time to
-            time in which case we will update the “Last Revised” date at the top
-            of these Airdrop Terms. The updated Airdrop Terms will be effective as
-            of the time of posting, or such later date as may be specified in the
-            updated Airdrop Terms. Your continued access or participation in the
-            Airdrop after the modifications have become effective will be deemed
-            your acceptance of the modified Airdrop Terms.{" "}
+          <p>
+            These Airdrop Terms and the General Terms contain the entire
+            agreement between you and the Organization regarding the Airdrop,
+            and supersede all prior and contemporaneous understandings between
+            the parties regarding the Airdrop. We may modify these Airdrop Terms
+            from time to time in which case we will update the “Last Revised”
+            date at the top of these Airdrop Terms. The updated Airdrop Terms
+            will be effective as of the time of posting, or such later date as
+            may be specified in the updated Airdrop Terms. Your continued access
+            or participation in the Airdrop after the modifications have become
+            effective will be deemed your acceptance of the modified Airdrop
+            Terms.{' '}
           </p>
         </div>
         <div className="[&>button]:w-[200px] flex min-h-fit flex-col items-stretch gap-4 py-5 px-10 text-sm">
--- frontend/components/modal/ModalWrapper.tsx
+++ frontend/components/modal/ModalWrapper.tsx
@@ -1,6 +1,6 @@
 export function ModalWrapper({ children }: { children: React.ReactNode }) {
   return (
-    <div className="fixed left-0 top-0 z-50 flex h-[100vh] w-full items-center justify-center p-6 before:absolute before:inset-0  before:bg-black before:bg-opacity-60 before:blur-[2px]">
+    <div className="fixed left-0 top-0 z-50 flex h-[100vh] w-full items-center justify-center p-6 before:absolute before:inset-0 before:bg-black before:bg-opacity-[35%] before:backdrop-blur-sm">
       {children}
     </div>
   )
