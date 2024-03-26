bff7a1d80409d2a2fbba282dbd85686a168252d7
matias martinez <matias@xlabs.xyz>
2024-03-26 15:08:48 -0300

    some debugging logs

--- backend/src/utils/secrets.ts
+++ backend/src/utils/secrets.ts
@@ -9,6 +9,7 @@ const client = new SecretsManagerClient({ region: config.aws.region })
 
 export async function getDispenserKey() {
   if (config.keys.dispenserGuard.key) {
+    console.log('Using dispenser guard key from config')
     return { key: JSON.parse(config.keys.dispenserGuard.key) }
   }
 
@@ -17,6 +18,7 @@ export async function getDispenserKey() {
 
 export async function getFundingKey() {
   if (config.keys.funding.key) {
+    console.log('Using funding key from config')
     return { key: JSON.parse(config.keys.funding.key) }
   }
 
@@ -28,8 +30,14 @@ export async function getSecret(secretName: string) {
     const command = new GetSecretValueCommand({ SecretId: secretName })
     const response = await client.send(command)
     if (response.SecretString) {
+      console.log(
+        `Retrieved secret: ${secretName}. ${response.SecretString.length} characters long`
+      )
       return JSON.parse(response.SecretString)
     } else if (response.SecretBinary) {
+      console.log(
+        `Retrieved binary secret: ${secretName}. ${response.SecretBinary.length} characters long`
+      )
       // For binary secrets, use Buffer to decode
       const buff = Buffer.from(response.SecretBinary.toString(), 'base64')
       return JSON.parse(buff.toString('ascii'))
