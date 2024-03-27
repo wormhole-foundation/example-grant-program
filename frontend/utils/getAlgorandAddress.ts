8e4aaff2918b6edd87197b26ed2b5714237d83fa
M-Picco <martin@xlabs.xyz>
2024-03-27 20:29:27 -0300

    trim algorand address tail ==== characters

--- frontend/utils/getAlgorandAddress.ts
+++ frontend/utils/getAlgorandAddress.ts
@@ -1,6 +1,8 @@
 import { base32encode } from '../claim_sdk'
 import { sha512_256 } from 'js-sha512'
 
+const ALGORAND_ADDRESS_LENGTH = 58;
+
 /**
  * Get Algorand address from 32 byte Ed25519 public key buffer
  *
@@ -13,5 +15,5 @@ export const getAlgorandAddress = (pubkey: Buffer): string => {
   }
   const checksum = sha512_256.array(pubkey)
   const address = Buffer.concat([pubkey, Buffer.from(checksum.slice(-4))])
-  return base32encode(address)
+  return base32encode(address).slice(0, ALGORAND_ADDRESS_LENGTH)
 }
