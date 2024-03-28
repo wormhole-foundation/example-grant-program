d0c54adb0cc96defbc57845edeea5faf4cf74ad5
solanoe <solanoepalacio@gmail.com>
2024-03-28 09:22:18 -0300

    new testnet deployment

--- token-dispenser/programs/token-dispenser/src/lib.rs
+++ token-dispenser/programs/token-dispenser/src/lib.rs
@@ -73,7 +73,7 @@ mod tests;
 
 mod ecosystems;
 
-declare_id!("wajpxnegR3qqtyeBwSRaGtQ9sxrTJWkA9kMCirrsudp");
+declare_id!("waNTLJXETasWVTP1qj84Zs6h7XY8qvzqmKd1QWqxyLB");
 
 const CONFIG_SEED: &[u8] = b"config";
 const RECEIPT_SEED: &[u8] = b"receipt";
