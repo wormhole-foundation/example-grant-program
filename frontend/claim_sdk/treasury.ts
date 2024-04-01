818d21b24b6c6dcff7e7590c01e8ee29b77718ce
Brian Monroy <brian.gmonroy98@gmail.com>
2024-04-01 16:04:44 -0300

    frontend prettier

--- frontend/claim_sdk/treasury.ts
+++ frontend/claim_sdk/treasury.ts
@@ -3,21 +3,21 @@ import { ClaimInfo } from './claim'
 import { PublicKey } from '@solana/web3.js'
 
 export const treasuries = [
-  new PublicKey("waTh9vNmRz8hHxQv1zW81ExKQ7fwaa1TR6tQT5fTkjg"),
-  new PublicKey("waTirsbdRyyxm8DFxH9Z9P5Pyrj74jJ7E74zqhmGX1k"),
-  new PublicKey("wAtMcoqwHb7obEL2USNXPqxg1CmJDSjjjsNcEm9bzCZ"),
-  new PublicKey("Wats1sM7W4UvxUuQPevGxexgcaFiaXMXxActKUmcvHx"),
-  new PublicKey("waTtgLt1WVRgoWYNZh97AYAUqoU4nN98jVThPdYQVwj"),
-  new PublicKey("WaTxa5w4TjYHRdHwG2zaN9ynA1c9z4J9TswsBCmzy4N"),
+  new PublicKey('waTh9vNmRz8hHxQv1zW81ExKQ7fwaa1TR6tQT5fTkjg'),
+  new PublicKey('waTirsbdRyyxm8DFxH9Z9P5Pyrj74jJ7E74zqhmGX1k'),
+  new PublicKey('wAtMcoqwHb7obEL2USNXPqxg1CmJDSjjjsNcEm9bzCZ'),
+  new PublicKey('Wats1sM7W4UvxUuQPevGxexgcaFiaXMXxActKUmcvHx'),
+  new PublicKey('waTtgLt1WVRgoWYNZh97AYAUqoU4nN98jVThPdYQVwj'),
+  new PublicKey('WaTxa5w4TjYHRdHwG2zaN9ynA1c9z4J9TswsBCmzy4N'),
 ]
 
 export const funders = [
-  new PublicKey("wAf15NnY6VVVQXDSdhp79LndkHXSD9BmcUhh4bqSSGp"),
-  new PublicKey("WafepN7ToYGS4WjaqtMV8g2q3YV1GgcoUZ4nvK4LhD8"),
-  new PublicKey("wAfmKAiAgtfywPhfRdLQ7uMeS2RW3kN57kodaRabX6V"),
-  new PublicKey("WAfN8P3NdYeTnnvFPKYUiveyqZSZtBboxUyrMSAzQc1"),
-  new PublicKey("wafydZpGcTLk3FYEQ5Q4wwSKJXMy3EMTngxeVEXG45e"),
-  new PublicKey("WafzGQMEqwABhMdNHH86yDRqzZQvK5rELK5Nrto97Va"),
+  new PublicKey('wAf15NnY6VVVQXDSdhp79LndkHXSD9BmcUhh4bqSSGp'),
+  new PublicKey('WafepN7ToYGS4WjaqtMV8g2q3YV1GgcoUZ4nvK4LhD8'),
+  new PublicKey('wAfmKAiAgtfywPhfRdLQ7uMeS2RW3kN57kodaRabX6V'),
+  new PublicKey('WAfN8P3NdYeTnnvFPKYUiveyqZSZtBboxUyrMSAzQc1'),
+  new PublicKey('wafydZpGcTLk3FYEQ5Q4wwSKJXMy3EMTngxeVEXG45e'),
+  new PublicKey('WafzGQMEqwABhMdNHH86yDRqzZQvK5rELK5Nrto97Va'),
 ]
 
 export function getClaimPayers(claimInfo: ClaimInfo): [PublicKey, PublicKey] {
