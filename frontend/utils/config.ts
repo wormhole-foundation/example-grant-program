20f0604a31e31a812196aa2d6c749f8f700ce1c3
M-Picco <martin@xlabs.xyz>
2024-04-02 10:59:09 -0300

    Add fallback ENDPOINT env variable

--- frontend/utils/config.ts
+++ frontend/utils/config.ts
@@ -3,10 +3,10 @@ import { clusterApiUrl } from '@solana/web3.js'
 const parseEndpoints = () => {
   let endpoints
   try {
-    endpoints = JSON.parse(process.env.ENDPOINTS!)
+    endpoints = JSON.parse(process.env.ENDPOINTS || process.env.ENDPOINT!)
   } catch (e) {
     // if parse fails, assume it's a single endpoint
-    endpoints = [process.env.ENDPOINTS || clusterApiUrl('devnet')]
+    endpoints = [process.env.ENDPOINTS || process.env.ENDPOINT || clusterApiUrl('devnet')]
   }
 
   if (!Array.isArray(endpoints)) {
